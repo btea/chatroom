e0ca57d8a09f142ac793bd1951b9658f5dd57b43
btea <2356281422@qq.com>
2021-02-24 11:32:45 +0800

    feat: css

--- src/components/Hello.tsx
+++ src/components/Hello.tsx
@@ -1,12 +1,12 @@
 import * as React from 'react';
-
+import './hello.css';
 export interface HelloProps {
     compiler: string;
     framework: string;
 }
 export const Hello = (props: HelloProps) => {
     return (
-        <h1>
+        <h1 className="demo">
             Hello from {props.compiler} and {props.framework}!
         </h1>
     );
