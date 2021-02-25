c6f50436603fcde942f6ad0aee24f14d6c38cb0f
btea <2356281422@qq.com>
2021-02-25 21:14:55 +0800

    feat: avatar

--- src/components/panelIn/MainPanel.tsx
+++ src/components/panelIn/MainPanel.tsx
@@ -1,6 +1,30 @@
-import React, { ReactElement } from 'react';
+import React, { ChangeEvent, ReactElement, useState } from 'react';
 import './panel.css';
 
 export function MainPanel(): ReactElement {
-    return <div className="panel-box"></div>;
+    const [src, setSrc] = useState('');
+
+    const avatar = (e: ChangeEvent) => {
+        const el = e.target as HTMLInputElement;
+        if (el && el.files) {
+            const file = el.files[0];
+            const reader = new FileReader();
+            reader.onload = () => {
+                setSrc(reader.result as string);
+            };
+            reader.readAsDataURL(file);
+        }
+        console.log(e.target);
+    };
+
+    return (
+        <div className="panel-box">
+            <label htmlFor="avatar" className="avatar-box">
+                <input type="file" name="avatar" id="avatar" onChange={avatar} />
+                <div className="avatar-con">
+                    <img src={src} alt="" />
+                </div>
+            </label>
+        </div>
+    );
 }
