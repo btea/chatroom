bdebda6dd8a02bfbc8445ede12ebcce939dee16f
btea <2356281422@qq.com>
2021-03-07 23:18:39 +0800

    feat: 头像放大的展示

--- src/components/friends/Friend.tsx
+++ src/components/friends/Friend.tsx
@@ -1,5 +1,6 @@
-import React, { ReactElement } from 'react';
+import React, { ReactElement, useState } from 'react';
 import styles from './friends.module.less';
+import ShowAvatar from './ShowAvatar';
 
 interface person {
     person: {
@@ -12,14 +13,24 @@ interface person {
 
 export default function Friend(props: person): ReactElement {
     const { person } = props;
+    const [showAvatar, switchShowAvatar] = useState(false);
+
     return (
         <div className={styles.friend}>
             <div className={styles.avatar}>
-                <img src={person.avatar} />
+                <img
+                    src={person.avatar}
+                    onClick={() => {
+                        switchShowAvatar(true);
+                    }}
+                />
             </div>
             <div className={styles['news-con']}>
                 <div className={styles['nick-name']}>{person.name}</div>
             </div>
+            {showAvatar && (
+                <ShowAvatar avatar={person.avatar} changeShow={switchShowAvatar}></ShowAvatar>
+            )}
         </div>
     );
 }
---
+++ src/components/friends/ShowAvatar.tsx
@@ -0,0 +1,24 @@
+import React, { ReactElement, SetStateAction } from 'react';
+import styles from './friends.module.less';
+
+interface avatar {
+    avatar: string;
+    changeShow: SetStateAction;
+}
+
+export default function ShowAvatar(props: avatar): ReactElement {
+    const { avatar, changeShow } = props;
+
+    return (
+        <div
+            className={styles['big-avatar']}
+            onClick={() => {
+                changeShow(false);
+            }}
+        >
+            <div className={styles['avatar-box']}>
+                <img src={avatar} alt="avatar" />
+            </div>
+        </div>
+    );
+}
