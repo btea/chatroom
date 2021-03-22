46daca763221c4a026511c8f75bffc3c0e2551ae
btea <2356281422@qq.com>
2021-03-22 22:49:18 +0800

    feat: 消息渲染样式调整

--- src/components/chat/NewsShow.tsx
+++ src/components/chat/NewsShow.tsx
@@ -1,4 +1,4 @@
-import React, { ReactElement } from 'react';
+import React, { ReactElement, useEffect, useRef } from 'react';
 import styles from './newsshow.module.less';
 
 interface news {
@@ -7,12 +7,26 @@ interface news {
 
 export default function NewsShow(props: news): ReactElement {
     const { info } = props;
+    const el = useRef(null);
+    useEffect(() => {
+        if (el.current) {
+            const _el = (el.current as unknown) as HTMLElement;
+            const _last = _el.lastChild as HTMLElement;
+            if (_last) {
+                _last.scrollIntoView({
+                    block: 'end',
+                    behavior: 'smooth'
+                });
+            }
+        }
+    });
     return (
-        <div className={styles['news-show']}>
+        <div className={styles['news-show']} ref={el}>
             {info &&
                 info.map((news, i) => {
                     return (
                         <div className={styles['content-box'] + ' ' + styles['left']} key={i}>
+                            <div className={styles['avatar']}></div>
                             <div className={styles['news-box']}>{news}</div>
                         </div>
                     );
