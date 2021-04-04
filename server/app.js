cb32038731d361019d053ff1c8d5ae12e8362588
btea <2356281422@qq.com>
2021-04-04 22:41:15 +0800

    feat: 添加客户端

--- server/app.js
+++ server/app.js
@@ -3,10 +3,19 @@
 
 const webSocketServer = require('ws').Server;
 let wsServer = new webSocketServer({ port: 2333 });
+let clients = [];
 wsServer.on('connection', function (socket) {
+    // console.log(socket);
+    clients.push(socket);
     socket.on('message', evt => {
-        console.log(evt);
-        socket.send('2233: ' + evt);
+        // socket.send('2233: ' + evt);
+        responseMsg(evt);
     });
     socket.send('有求皆苦，无欲则刚');
 });
+
+function responseMsg(msg) {
+    clients.forEach(ws => {
+        ws.send(msg);
+    });
+}
