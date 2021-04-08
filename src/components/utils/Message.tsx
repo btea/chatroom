cedc469f21c1980def1e27014c1f01dbf2b03e7f
btea <2356281422@qq.com>
2021-04-08 22:38:47 +0800

    feat: tip功能完善

--- src/components/utils/Message.tsx
+++ src/components/utils/Message.tsx
@@ -9,17 +9,21 @@ interface MessageProps {
 
 export default function Message(props: MessageProps): void {
     const type = props.type || 'info';
-    let boxName = styles['msg-box'] + ' ' + styles[type] + ' ' + styles['show'];
+    let boxName = styles['msg-box'] + ' ' + styles[type];
     const duration = props.duration || 3000;
     const div = document.createElement('div');
     div.innerText = props.msg;
     div.className = boxName;
+    const t = 500;
     setTimeout(() => {
         boxName = styles['msg-box'] + ' ' + styles[type];
         div.className = boxName;
         setTimeout(() => {
-            document.body.appendChild(div);
-        }, 1000);
-    }, duration);
+            document.body.removeChild(div);
+        }, t);
+    }, duration + t);
+    setTimeout(() => {
+        div.className = styles['msg-box'] + ' ' + styles[type] + ' ' + styles['show'];
+    }, t);
     document.body.appendChild(div);
 }
