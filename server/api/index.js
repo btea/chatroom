15af9bcf1f58b6e75befd1d945cc1bdb8abd5e39
btea <2356281422@qq.com>
2021-04-08 22:17:38 +0800

    feat: 添加api

---
+++ server/api/index.js
@@ -0,0 +1,25 @@
+/**
+ * 解析请求参数
+ * @param req 请求实例
+ * @param res 响应实例
+ */
+function parseParams(req, res) {
+    const { url } = req;
+    let params = '';
+    req.on('data', chunk => {
+        params += chunk;
+    });
+    req.on('end', () => {
+        console.log(params);
+    });
+}
+
+/**
+ * 注册账户
+ * @param name 用户名
+ * @param avatar 用户头像
+ * @param id 用户id、用来通信的标识
+ */
+// function startSign(name, avatar, id) {}
+
+module.exports = {};
