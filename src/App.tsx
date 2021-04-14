dd2f4f9e0f0f705cd4aa4231480e9c3895796496
btea <2356281422@qq.com>
2021-04-14 22:10:58 +0800

    feat: 添加注册页面

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React, { ReactElement } from 'react';
 import { MainPanel } from './components/panelIn/MainPanel';
+import Register from './components/panelIn/Register';
 import './app.css';
 
 export function MainBox(): ReactElement {
     return (
         <div className="main-in">
-            <MainPanel />
+            {/* <MainPanel /> */}
+            <Register></Register>
         </div>
     );
 }
--- src/components/panelIn/Register.tsx
+++ src/components/panelIn/Register.tsx
@@ -1,13 +1,19 @@
 import React, { ReactElement } from 'react';
 import styles from './register.module.less';
-import { MainPanel } from './MainPanel';
 // 用户注册模块
 
 export default function Register(): ReactElement {
     return (
         <div className={styles['register-box']}>
-            <div>注册</div>
-            <MainPanel></MainPanel>
+            <div className={styles['nickname']}>
+                <input type="text" className={styles['input']} />
+            </div>
+            <div className={styles['password']}>
+                <input type="password" className={styles['input']} />
+            </div>
+            <div className={styles['btns']}>
+                <button className={styles['btn-text']}>注册</button>
+            </div>
         </div>
     );
 }
