353e5f83ed18245a54b24dbe1d0e33ea0ffcf2fe
btea <2356281422@qq.com>
2021-04-16 10:51:19 +0800

    feat: api

--- server/api/apiList.js
+++ server/api/apiList.js
@@ -31,6 +31,7 @@ function notFound(params, res) {
 function registerUser(params, res) {
     if (params) {
         params = JSON.parse(params);
+        params = Object.assign({ sign: '天空高远，大风吟唱~' }, params);
     }
     const db = startLink('userInfo');
 
@@ -73,44 +74,59 @@ function login(params, res) {
 }
 
 /**
- * 修改用户名、密码
+ * 修改用户密码、用户名不能修改
  */
+function editUserPwd(params, res) {}
 
 /**
  * 修改用户昵称、签名、背景图片等等
  */
+function editUserInfo(params, res) {}
 
 /**
  * 添加好友接口
+ * @param params {object}
+ * @param params.name 用户名
  */
+function addFriend(params, res) {}
 
 /**
  * 通过添加好友请求接口
  */
+function agreeFriend(params, res) {}
 
 /**
  * 拒绝好友请求接口
  */
+function refuseFreind(params, res) {}
 
 /**
  * 获取好友列表接口
  */
+function getFriendsList(params, res) {}
 
 /**
  * 获取进行操作的列表接口，比如添加好友。。。
  */
+function getOperations(params, res) {}
 
 /**
  * 获取已经添加的群聊的接口
  */
+function getChatRoomList(params, res) {}
 
 /**
  * 通过房间id获取群的相关信息
  */
-
+function getChatRoomInfo(params, id) {}
 /**
- * 通过用户id查询用户相关信息
+ * 通过房间id获取房间名、说明等信息
  */
+function getChatRoomMsg(params, res) {}
+/**
+ * 通过房间id获取相应成员信息（头像，名字等）
+ */
+function getChatRoomMember(params, res) {}
 
 module.exports = {
     '/': defaultResponse,
