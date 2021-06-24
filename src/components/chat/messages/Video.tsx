8cff8018cc449d9ea9e2b07591d80bcff3b528da
btea <2356281422@qq.com>
2021-06-24 17:59:10 +0800

    feat: type video

---
+++ src/components/chat/messages/Video.tsx
@@ -0,0 +1,10 @@
+import React, { ReactElement } from "react";
+import styles from './video.module.less';
+
+export default function Video(props: InfoType.info): ReactElement {
+    return (
+        <div className={styles['video-msg']}>
+            
+        </div>
+    )
+}
