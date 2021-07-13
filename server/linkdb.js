1bb19db3eaf542a7e6eb200592b48cb88d99ad33
btea <2356281422@qq.com>
2021-07-13 09:37:00 +0800

    feat: remove nedb

--- server/linkdb.js
+++ server/linkdb.js
@@ -2,7 +2,8 @@
  * 链接数据库
  * @param name {string} 数据库的名字
  */
-let DataStore = require('nedb');
+// let DataStore = require('nedb');
+const { MongoClient } = require('mongodb');
 function linkStart(name) {
     if (!name) {
         throw Error('请输入有效的数据库名字');
@@ -10,7 +11,10 @@ function linkStart(name) {
     if (typeof name !== 'string') {
         throw TypeError('参数 name 应该为一个字符串');
     }
-    return new DataStore({ filename: `./info/${name}`, autoload: true });
+    const url = 'mongodb://localhost:2233';
+    const client = new MongoClient(url);
+    return client;
+    // return new DataStore({ filename: `./info/${name}`, autoload: true });
 }
 
 module.exports = linkStart;
