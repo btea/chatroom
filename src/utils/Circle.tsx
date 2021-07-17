170c3db8651203dec98dbde3465de7e452805eb8
btea <2356281422@qq.com>
2021-07-17 15:01:32 +0800

    feat: 点击效果随机

--- src/utils/Circle.tsx
+++ src/utils/Circle.tsx
@@ -1,9 +1,10 @@
 import { randomColor } from '@btea/utils';
 
-const w = window.innerWidth;
-const h = window.innerHeight;
+const w = 200;
+const h = 200;
 
 export default function createCircle(x: number, y: number): void {
+    let opa = 0.6;
     const initStyle = {
         width: '100px',
         height: '100px',
@@ -11,16 +12,21 @@ export default function createCircle(x: number, y: number): void {
         background: 'rgba(102, 204, 255, .4)',
         position: 'absolute',
         left: '',
-        top: ''
+        top: '',
+        transform: 'translate(-50%, -50%)',
+        'z-index': 0
     };
-    console.log(randomColor());
     const el = document.createElement('div');
     initStyle.left = `${x}px`;
     initStyle.top = `${y}px`;
+    const rgb = hexToRgb(randomColor());
+    initStyle.background = `${rgb.slice(0, -1)},${opa})`;
     const timer = window.setInterval(() => {
         Object.defineProperties(el.style, Object.getOwnPropertyDescriptors(initStyle));
         const _w = parseInt(el.style.width);
         const _h = parseInt(el.style.height);
+        opa -= 0.05;
+        initStyle.background = `${rgb.slice(0, -1)},${opa})`;
         initStyle.width = `${_w + 10}px`;
         initStyle.height = `${_h + 10}px`;
         if (_w + 10 >= w && _h + 10 >= h) {
@@ -28,8 +34,16 @@ export default function createCircle(x: number, y: number): void {
             document.body.removeChild(el);
             document.body.removeChild(comment);
         }
-    });
+    }, 1000 / 60);
     const comment = new Comment(`setInterval ${timer}`);
     document.body.appendChild(comment);
     document.body.appendChild(el);
+}
+
+function hexToRgb(color: string): string {
+    color = color.slice(1);
+    const r = parseInt(color.slice(0, 2), 16);
+    const g = parseInt(color.slice(2, 4), 16);
+    const b = parseInt(color.slice(-2), 16);
+    return `rgba(${r}, ${g}, ${b})`;
 }
