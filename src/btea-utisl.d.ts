2034db0e920dbb972a0b61a1cbf1abe0700aa3c0
btea <2356281422@qq.com>
2021-07-17 08:08:40 +0800

    type: 添加类型声明

---
+++ src/btea-utisl.d.ts
@@ -0,0 +1,15 @@
+declare module '@btea/utils' {
+    export function isLeapyear(year: string | number): boolean;
+    export function getMonthTotal(year: string | number, month: string | number): number;
+    export function formatDate(date: Date | string | number, format: string): string;
+    export function randomColor(): string;
+    export function isEven(val: unknown): boolean;
+    export function isOdd(val: unknown): boolean;
+    export function randomNum(max: number, min: number, isInteger: boolean): number;
+    export function isString(val: unknown): boolean;
+    export function isNumber(val: unknown): boolean;
+    export function isArray(val: unknown): boolean;
+    export function isObject(val: unknown): boolean;
+    export function isTruly(val: unknown): boolean;
+    export function getType(val: unknown): string;
+}
